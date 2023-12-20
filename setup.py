ad42b7ddd579ddf04154abb451d87fa7c5623f14
asbpintu <asbpintu@gmail.com>
2023-12-20 23:28:44 +0530

    setup & required packages installed

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Stroke-Risk-Prediction"
+AUTHOR_USER_NAME = "asbpintu"
+SRC_REPO = "stroke_risk"
+AUTHOR_EMAIL = "asbpintu@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="This Project predict the Risk of Stroke for a Patient",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
