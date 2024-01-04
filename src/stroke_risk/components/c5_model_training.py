d2db1ba50d80d0a9534f2683b599a2b5746b7a50
asbpintu <asbpintu@gmail.com>
2024-01-04 22:06:38 +0530

    class imbalance fixed

--- src/stroke_risk/components/c5_model_training.py
+++ src/stroke_risk/components/c5_model_training.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import os
 from sklearn.preprocessing import StandardScaler
+from imblearn.combine import SMOTETomek
 from sklearn.ensemble import RandomForestClassifier
 import joblib
+from pathlib import Path
 from stroke_risk import logger
 
 from stroke_risk.entity.config_entity import ModelTrainingConfig
+from stroke_risk.utils.common import create_directories
 
 
 class ModelTraining:
@@ -19,8 +22,12 @@ class ModelTraining:
         x_train = train_data.drop([self.config.target_column], axis=1)
         y_train = train_data[self.config.target_column]
 
-        scaler = StandardScaler()
-        x_train = scaler.fit_transform(x_train)
+        # scaler = StandardScaler()
+        # x_train = scaler.fit_transform(x_train)
+
+        imb = SMOTETomek(sampling_strategy='minority', random_state=42)
+        x_train , y_train = imb.fit_resample(x_train,y_train)
+
 
         model = RandomForestClassifier(
             n_estimators = self.config.n_estimators,
@@ -36,4 +43,7 @@ class ModelTraining:
         model.fit(x_train, y_train)
 
         joblib.dump(model, os.path.join(self.config.root_dir, self.config.model_name))
+        create_directories(['model'])
+        joblib.dump(model,Path('model\model.joblib'))
         logger.info('Model Trained')
+        logger.info('Model saved at model/model.joblib')
