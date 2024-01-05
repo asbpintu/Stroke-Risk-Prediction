427e9ec609fcd9ec49ef587d2c8f2a564209042a
asbpintu <asbpintu@gmail.com>
2024-01-05 19:58:25 +0530

    train with diff params

--- src/stroke_risk/components/c6_model_evaluation.py
+++ src/stroke_risk/components/c6_model_evaluation.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score,accuracy_score
 from urllib.parse import urlparse
 import mlflow
 import mlflow.sklearn
@@ -31,7 +31,8 @@ class ModelEvaluation:
         rmse = np.sqrt(mean_squared_error(actual, pred))
         mae = mean_absolute_error(actual, pred)
         r2 = r2_score(actual, pred)
-        return rmse, mae, r2
+        acc = accuracy_score(actual, pred)
+        return rmse, mae, r2, acc
     
 
 
@@ -52,10 +53,10 @@ class ModelEvaluation:
 
             y_pred = model.predict(x_test)
 
-            (rmse, mae, r2) = self.eval_metrics(y_test, y_pred)
+            (rmse, mae, r2, acc) = self.eval_metrics(y_test, y_pred)
             
             # Saving metrics local
-            scores = {"rmse": rmse, "mae": mae, "r2": r2}
+            scores = {"rmse": rmse, "mae": mae, "r2": r2, "acc":acc}
             save_json(path=Path(self.config.metric_file_name), data=scores)
 
             mlflow.log_params(self.config.all_params)
@@ -63,6 +64,7 @@ class ModelEvaluation:
             mlflow.log_metric("rmse", rmse)
             mlflow.log_metric("r2", r2)
             mlflow.log_metric("mae", mae)
+            mlflow.log_metric("acc", acc)
 
 
             if tracking_url_type_store != "file":
